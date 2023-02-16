fc48e2f29659a5cc72f99c18412c47fc53a51991
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-02-16 18:40:09 +0100

    Add project files.

---
+++ HelloWorld_Xamarin/HelloWorld_Xamarin/GreetPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld_Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            Content = new Label
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Text = "Hello World"
+            };
+        }
+
+        
+    }
+}
