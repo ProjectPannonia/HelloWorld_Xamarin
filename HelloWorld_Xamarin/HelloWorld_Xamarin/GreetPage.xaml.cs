c9200114c1bb8ecd84bb8ec351f80da7dc29d002
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-02-21 20:42:14 +0100

    Handling device differencies

--- HelloWorld_Xamarin/HelloWorld_Xamarin/GreetPage.xaml.cs
+++ HelloWorld_Xamarin/HelloWorld_Xamarin/GreetPage.xaml.cs
@@ -11,6 +11,16 @@ namespace HelloWorld_Xamarin
         {
             InitializeComponent();
             slider.Value = 0.5;
+
+            /* Target device handler - old way
+            if (Device.OS == TargetPlatform.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+            else if (Device.OS == TargetPlatform.Android)
+                Padding = new Thickness(10, 20, 0, 0);
+            else if (Device.OS == TargetPlatform.WinPhone)
+                Padding = new Thickness(30, 20, 0, 0);
+            */
+            /* Target device handler - new, easier way */
             //label.Text = String.Format("Value is {0:F2}", slider.Value);
         }
 
